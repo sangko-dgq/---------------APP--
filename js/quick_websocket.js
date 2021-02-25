c502bbbe05eb461352407be6ad4e95a502ed13a5
sangko-dgq <2201225826@qq.com>
2021-02-25 14:28:24 +0800

    master

--- js/quick_websocket.js
+++ js/quick_websocket.js
@@ -20,80 +20,62 @@ btn_quickConnect.addEventListener("click", function() {
 	mui.toast("Quick Connect.....");
 		
 	console.log("服务器地址为：" + url);	
+	console.log("登录json为:" + json_login);
 	
 	// 1. Connect+Login
 	ConnectAndLoginWebsocket(url);
+
 });
 
 //create websocket
 function ConnectAndLoginWebsocket(url) 
 {
+/*********连接ws**********************************************/
 	var ws = new WebSocket(url);
-
-	
 	ws.onopen = function() 
 	{
 		// console.log("Connected!");
 		me_console.innerText = "Connected!";
 	    Check_ws_state(ws);//通信检测
+		
 	};
-	
-	//收到消息，说明连接成功
+	//收到连接成功的json回复消息，说明连接成功
 	ws.onmessage = function(evt)
 	{
 		var received_msg = evt.data;
-		console.log(received_msg);
 		me_console.innerText = received_msg;
-			
+		console.log(received_msg);
+		
+        ws.send('{"M":"status"}');
+		if(received_msg = {"M":"connected"})
+		{
+			console.log(received_msg);
+		}
+/*********登录ws上创建的设备**********************************************/
 		//发送json登录指令
 		//{"M":"checkin","ID":"xx1","K":"xx2"}	
-		console.log(json_login);
-		ws.send(json_login);
-
-		
-		//SEND MESSAGE
-		
-		//{"M":"say","ID":"xx1","C":"xx2","SIGN":"xx3"}	
-		var fanSpeed_range = document.getElementById("fanSpeed_range");
-		//滑动条滑动监听
-		fanSpeed_range.addEventListener("touchmove", function()
+		if(received_msg = {"M":"connected"})
 		{
-			//注意不能直接将该条放在监听外面，他会默认等于value的默认值
-			var json_rangeToSend = '{"M":"say","ID":"'+userID+'","C":"'+fanSpeed_range.value+'","SIGN":"xx3"}';
-			ws.send(json_rangeToSend);
-		});	
+			ws.send(json_login);
+			//查询连接登录状态(在服务器返回结果中：connected代表已连接服务器尚未登录，checked代表已连接且登录成功)
+			ws.send('{"M":"status"}');
+			//登录成功
+			if(received_msg = {"M":"checked"})
+			{
+				console.log(received_msg);
+				SetCirleState_teal();
+/*****************************检测到登录成功后，连接数据模块***************************/
+                Hand_fanSpeed_range(ws); // 手动滑块控制风机速度模块
+                
+			}
+		}
 	};
-	
-	//如果收到返回登录消息，说明登录登录成功
-	//将状态圆变成绿色
-	if(me_console.innerText !=' {"M":"WELCOME TO BIGIOT"}' && me_console.innerText != '{"M":"ping"}')
-	{
-		// console.log("All is done!"); 
-		//修改cirle_state的样式类名为green
-		cirle_state.className = "ui teal right floated header";
-	}
-	
-	//连接手动关闭时，自动调用（注意：1min无send这种,自动关闭不会调用）
-		ws.onclose = function()
-		{
-			// SetCirleState_red();
-			console.log("Nothing sent within 1min,Connection is Broken!");
-		};
 }
 
+/*************************************模块封装*******************************************/
 
-
-//2, 由于函数只会调用一次，所以单独把断线状态球恢复为red方法写在点击时间的外面
-//设备登录后，如果在1分钟内无数据传送，连接将被自动关闭。状态球恢复为红色状态
-	 // setInterval(Check_broken_cirleState, 10000); //利用该函数可以反复执行,10s
-	 function SetCirleState_red()
-	 {
-			// console.log("Nothing sent within 1min,Connection is Broken!"); 
-			me_console.innerText = "Nothing sent within 1min,Connection is Broken!";
-			cirle_state.className = "ui red right floated header";
-	 }
-	 
-	 // 函数封装模块： 通信状态实时检测（不包含登录检测）
+/**********************************网络状态检测模块封装**************************************/
+	 // 通信状态实时检测（不包含登录检测）
 	 function Check_ws_state(ws)
 	 {
 	 	var ws_state = ws.readyState;
@@ -109,8 +91,37 @@ function ConnectAndLoginWebsocket(url)
 	 			break;
 	 		case 3:
 	 			console.log("ws state: ws closed!");
+				SetCirleState_red();
 	 			break;
 	 		default:
 	 			break;
 	 	}
 	 }
+
+/**********************************UI动态模块封装*******************************************/
+	 // setInterval(Check_broken_cirleState, 10000); //利用该函数可以反复执行,10s
+	 function SetCirleState_red()    //未连接或未登录，状态球变为红色
+	 {
+			// console.log("Nothing sent within 1min,Connection is Broken!"); 
+			// me_console.innerText = "Nothing sent within 1min,Connection is Broken!";
+			cirle_state.className = "ui red right floated header";
+	 }
+	 function SetCirleState_teal()   //已连接且已登录，状态球变为绿茶色
+	 {
+		 cirle_state.className = "ui teal right floated header";
+	 }
+	 
+ /**********************************数据模块封装*******************************************/
+	 //风机滑块手动调速模块
+     function Hand_fanSpeed_range(ws)
+	 {
+		 var fanSpeed_range = document.getElementById("fanSpeed_range");
+		 //滑动条滑动监听
+		 fanSpeed_range.addEventListener("touchmove", function()
+		 {
+		 	//注意不能直接将该条放在监听外面，他会默认等于value的默认值
+		 	var json_rangeToSend = '{"M":"say","ID":"'+userID+'","C":"'+fanSpeed_range.value+'","SIGN":"xx3"}';
+		 	ws.send(json_rangeToSend);
+		 });	
+	 }
+	 	
